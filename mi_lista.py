59b9952966ae50346573bb845a0acc96437286e7
Balderrama-Karla-5J <a.22308051281149@cbtis128.edu.mx>
2024-09-09 10:14:03 -0600

    Add files via upload
    agregue archivos de ejemplo

---
+++ mi_lista.py
@@ -0,0 +1,17 @@
+print("Ejemplo de lista")
+arcoiris =["verde" ,"azul" ,"morado"]
+print(arcoiris)
+longitud =len(arcoiris)
+print("Elementos que contiene la lista arcoiris ", longitud)
+print(f"Elementos que contiene la lista arcoiris v2  {longitud}")
+print("accediendo a un elemento de la lista")
+print(arcoiris[1])
+print(f"Elemento en la posicion 0 es {arcoiris[0]}")
+print("agregar a un elemento de la lista")
+print(arcoiris)
+arcoiris.append("naranja")
+print(arcoiris)
+print("Listar elementos en la lista ciclo for ")
+for balderrama in arcoiris:
+    print(balderrama)
+
---
+++ mi_tuplas.py
@@ -0,0 +1,9 @@
+print("Ejemplo de tuplas")
+canciones=("Te amo", "El NoNoa", "Amor eterno")
+print(canciones)
+y = list(canciones)
+print(y)
+y[1] = "La puerta Negra"
+x = tuple(y)
+
+print(x)
