78264e7cc0dbdcf89a4028e450019e5834129190
Balderrama-Karla-5J <a.22308051281149@cbtis128.edu.mx>
2024-09-11 07:44:13 -0600

    Add files via upload
    agregue ejemplos de conjunto y diccionario

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("Ejemplo de conjuntos set")
+tipodeportes = {"futbol", "Tenis", "Basquetbol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop for")
+print(tipodeportes)
+for balderrama in tipodeportes:
+    print(balderrama)
---
+++ mi_diccionario.py
@@ -0,0 +1,11 @@
+biblioteca = {
+  "Libro": "Sherlock Holmes",
+  "Autor": "Arthur Conan Doyle",
+  "año": 1907
+}
+print(biblioteca)
+for x in biblioteca:
+ print(x)
+  
+for x in biblioteca:
+      print(biblioteca[x])
